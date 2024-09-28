59e971c1935382b521f9a4f7f5325270a8542977
Will Velida <willvelida@hotmail.co.uk>
2024-09-28 21:22:10 +1000

    Adding basic infra for Auth service
    Signed-off-by: Will Velida <willvelida@hotmail.co.uk>

---
+++ infra/auth/main.tf
@@ -0,0 +1,55 @@
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+}
+
+data "azurerm_container_app_environment" "env" {
+  name = var.env_name
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+data "azurerm_user_assigned_identity" "msi" {
+  name = var.uai_name
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+data "azurerm_container_registry" "acr" {
+  name = var.acr_name
+    resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+resource "azurerm_container_app" "app" {
+  name = var.aca_app_name
+  resource_group_name = data.azurerm_resource_group.rg.name
+  tags = var.tags
+  revision_mode = "Multiple"
+  container_app_environment_id = data.azurerm_container_app_environment.env.id
+
+  template {
+    container {
+      name = var.aca_app_name
+      image = "mcr.microsoft.com/k8se/quickstart:latest"
+      cpu = 0.25
+      memory = "0.5Gi"
+    }
+    min_replicas = 0
+    max_replicas = 3
+  }
+
+  ingress {
+    external_enabled = true
+    target_port = 80
+    traffic_weight {
+        percentage = 100
+    }
+  }
+
+  registry {
+    server = data.azurerm_container_registry.acr.login_server
+    identity = data.azurerm_user_assigned_identity.msi.id
+  }
+
+  identity {
+    type = "UserAssigned"
+    identity_ids = [data.azurerm_user_assigned_identity.msi.id]
+  }
+}
---
+++ infra/auth/tfvars/terrafrom.tfvars
@@ -0,0 +1,11 @@
+resource_group_name = "rg-biotrackr-prod-ae"
+env_name = "env-biotrackr-prod-ae"
+uai_name = "uai-biotrackr-prod-ae"
+acr_name = "acrbiotrackrprodae"
+aca_app_name = "biotrackr-auth"
+tags = {
+  environment = "Production"
+  owner = "willvelida"
+  Application = "biotrackr"
+  Component = "Auth"
+}
