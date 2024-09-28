b071d249fc11509a7f79d1fd40bd977f84e5241f
Will Velida <willvelida@hotmail.co.uk>
2024-09-28 17:06:54 +1000

    Adding modules

---
+++ infra/modules/monitor-action-group/variables.tf
@@ -0,0 +1,29 @@
+variable "amg_name" {
+  description = "The name of the Monitor Action Group"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group in which the resources should be created"
+  type        = string
+}
+
+variable "amg_short_name" {
+  description = "The short name of the Monitor Action Group"
+  type        = string
+}
+
+variable "tags" {
+  description = "A map of tags to add to the resources"
+  type        = map(string)
+}
+
+variable "email_receiver_name" {
+  description = "The name of the email receiver"
+  type        = string
+}
+
+variable "email_address" {
+  description = "The email address of the email receiver"
+  type        = string
+}
