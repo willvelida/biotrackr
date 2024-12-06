993ff5c4144aee5a480ffac12c0b955ac5f51f1e
Will Velida <willvelida@hotmail.co.uk>
2024-12-06 14:54:59 +1100

    Removing ACR task, and add pipeline for task
    Signed-off-by: Will Velida <willvelida@hotmail.co.uk>

---
+++ infra/scripts/purge-old-images.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Variables
+ACR_NAME=${ACR_NAME}
+RESOURCE_GROUP=${RESOURCE_GROUP}
+
+# Get the list of repositories
+repositories=$(az acr repository list --name $ACR_NAME --resource-group $RESOURCE_GROUP --output tsv)
+
+# Loop through each repository and purge old images
+for repository in $repositories; do
+    echo "Purging old images in repository: $repository"
+    az acr run --registry $ACR_NAME --cmd "acr purge --filter '$repository:.*' --ago 0d --keep 2" /dev/null
+done
