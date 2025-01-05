30b75d549bfb2a38de94c601a2dbcf7be77f664f
Will Velida <willvelida@hotmail.co.uk>
2025-01-05 16:55:22 +1100

    Adding code coverage exclusion for ActivityDocument
    Signed-off-by: Will Velida <willvelida@hotmail.co.uk>

--- src/Biotrackr.Activity.Api/Biotrackr.Activity.Api/Models/ActivityDocument.cs
+++ src/Biotrackr.Activity.Api/Biotrackr.Activity.Api/Models/ActivityDocument.cs
@@ -1,7 +1,9 @@
 ﻿using Biotrackr.Activity.Api.Models.FitbitEntities;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Biotrackr.Activity.Api.Models
 {
+    [ExcludeFromCodeCoverage]
     public class ActivityDocument
     {
         public string Id { get; set; }
