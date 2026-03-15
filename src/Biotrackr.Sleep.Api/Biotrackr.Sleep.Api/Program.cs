55463581a8dc2ab435f0470fe9b23a8eb70d3b73
Will Velida <willvelida@hotmail.co.uk>
2026-03-15 21:08:47 +1100

    feat: add OpenTelemetry instrumentation to Biotrackr.Sleep.Api
    - Add Azure.Monitor.OpenTelemetry.Exporter and OpenTelemetry packages to csproj
    - Configure tracing, metrics, and logging with Azure Monitor exporter in Program.cs
    - Add ExcludeFromCodeCoverage on Program class for coverage threshold
    - Add appInsightsName param and applicationinsightsconnectionstring env var to infra Bicep
    - Add dummy App Insights connection string to integration test factory
    
    Signed-off-by: Will Velida <willvelida@hotmail.co.uk>

--- src/Biotrackr.Sleep.Api/Biotrackr.Sleep.Api/Program.cs
+++ src/Biotrackr.Sleep.Api/Biotrackr.Sleep.Api/Program.cs
@@ -1,10 +1,22 @@
 using Azure.Identity;
+using Azure.Monitor.OpenTelemetry.Exporter;
 using Biotrackr.Sleep.Api.Configuration;
 using Biotrackr.Sleep.Api.Extensions;
 using Biotrackr.Sleep.Api.Repositories;
 using Biotrackr.Sleep.Api.Repositories.Interfaces;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System.Diagnostics.CodeAnalysis;
+
+var resourceAttributes = new Dictionary<string, object>
+{
+    { "service.name", "Biotrackr.Sleep.Api" },
+    { "service.version", "1.0.0" }
+};
+var resourceBuilder = ResourceBuilder.CreateDefault().AddAttributes(resourceAttributes);
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -52,6 +64,39 @@ builder.Services.AddOpenApi();
 
 builder.Services.AddHealthChecks();
 
+var appInsightsConnectionString = builder.Configuration["applicationinsightsconnectionstring"];
+
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracing =>
+    {
+        tracing.SetResourceBuilder(resourceBuilder)
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddAzureMonitorTraceExporter(options =>
+            {
+                options.ConnectionString = appInsightsConnectionString;
+            });
+    })
+    .WithMetrics(metrics =>
+    {
+        metrics.SetResourceBuilder(resourceBuilder)
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddAzureMonitorMetricExporter(options =>
+            {
+                options.ConnectionString = appInsightsConnectionString;
+            });
+    });
+
+builder.Logging.AddOpenTelemetry(log =>
+{
+    log.SetResourceBuilder(resourceBuilder);
+    log.AddAzureMonitorLogExporter(options =>
+    {
+        options.ConnectionString = appInsightsConnectionString;
+    });
+});
+
 var app = builder.Build();
 
 app.MapOpenApi();
@@ -60,3 +105,6 @@ app.RegisterSleepEndpoints();
 app.RegisterHealthCheckEndpoints();
 
 app.Run();
+
+[ExcludeFromCodeCoverage]
+public partial class Program { }
